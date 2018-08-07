3cdf7b03b10c8bb3b447e939c6bb988769611b69
rargarw <rargarw@tranunion.com>
2018-08-06 21:13:31 -0500

    chore: Adding cmake

---
+++ cmake-build-debug/CMakeFiles/googleTestDemo.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rargarw/CLionProjects/googleTestDemo/test/SimpleTest.cpp" "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/CMakeFiles/googleTestDemo.dir/test/SimpleTest.cpp.o"
+  "/Users/rargarw/CLionProjects/googleTestDemo/test/SquareRootTest.cpp" "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/CMakeFiles/googleTestDemo.dir/test/SquareRootTest.cpp.o"
+  "/Users/rargarw/CLionProjects/googleTestDemo/test/StringTest.cpp" "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/CMakeFiles/googleTestDemo.dir/test/StringTest.cpp.o"
+  "/Users/rargarw/CLionProjects/googleTestDemo/test/basicTest.cpp" "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/CMakeFiles/googleTestDemo.dir/test/basicTest.cpp.o"
+  "/Users/rargarw/CLionProjects/googleTestDemo/test/main.cpp" "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/CMakeFiles/googleTestDemo.dir/test/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest-src/googlemock/include"
+  "../src"
+  "googletest-src/googletest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/googletest-build/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/rargarw/CLionProjects/googleTestDemo/cmake-build-debug/googletest-build/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/googleTestDemo.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/googleTestDemo.dir/test/basicTest.cpp.o"
+  "CMakeFiles/googleTestDemo.dir/test/main.cpp.o"
+  "CMakeFiles/googleTestDemo.dir/test/SimpleTest.cpp.o"
+  "CMakeFiles/googleTestDemo.dir/test/SquareRootTest.cpp.o"
+  "CMakeFiles/googleTestDemo.dir/test/StringTest.cpp.o"
+  "googleTestDemo.pdb"
+  "googleTestDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/googleTestDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
